8f30e8fac9ce181662727409fb8ab61240766243
lihkir <likyalpha@gmail.com>
2021-03-08 16:45:08 -0500

    Caja y Bigotes usando Spyder
    Caja y Bigotes usando Spyder

---
+++ Estadistica1_2021A/ejemplo_caja_bigotes.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+from numpy.random import seed
+from numpy.random import randn
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style="whitegrid")
+
+csv_data = pd.read_csv("C:\\Users\\likyb\\Documents\\GitHub\\Uninorte\\Estadistica1_2021A\\TGLS.csv")
+print(csv_data.head())
+
+OpenList = csv_data.Open.tolist()
+ax=sns.boxplot(x=OpenList)
+plt.show()
